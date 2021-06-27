08884b804fd6a6b5ed02e7ed992e849fdf4dc5fb
Celio Iyama <celio.iyama@gmail.com>
2021-06-27 14:10:38 -0300

    Order, Instant, ISO 8601 PART-2

---
+++ src/main/java/com/projetoa/services/OrderService.java
@@ -0,0 +1,27 @@
+package com.projetoa.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.projetoa.entities.Order;
+import com.projetoa.entities.User;
+import com.projetoa.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+
+	@Autowired
+	private OrderRepository repository;
+	
+	public List<Order> findALL() {
+		return repository.findAll();
+	}
+	
+	public Order findById(Long id) {
+		Optional<Order> obj = repository.findById(id);
+		return obj.get();
+	}
+}
